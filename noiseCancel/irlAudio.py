127a8c344ad41efc326e1f049cf4a863e1f56299
Marcus Good <52837540+marcusgood@users.noreply.github.com>
2020-09-11 23:00:12 -0400

    Planning notes for IRL/actual audio

--- noiseCancel/irlAudio.py
+++ noiseCancel/irlAudio.py
@@ -1,2 +1,6 @@
 import pyaudio as pa
 import numpy as np
+
+#Use pyaudio, and third party package to access microphone thru MacOS
+#Record audio file?, then plot sound using matplotlib?
+#Go from there a sort out which is white noise and which is noise from data?
